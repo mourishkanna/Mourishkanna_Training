2886d20048e9407c75aca4e9939648c32430510d
Mourish Kanna <mourishkumar1201@gmail.com>
2024-06-21 00:52:36 +0530

    Recycler View: Creating XML, Data and Handle

--- app/src/main/java/com/example/mourishkannatraining/HomeActivity.kt
+++ app/src/main/java/com/example/mourishkannatraining/HomeActivity.kt
@@ -4,32 +4,31 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
-import android.widget.ListView
 import android.widget.Spinner
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
-class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
+class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener{
+    var data = arrayOf("Tamil Nadu","Tamil","Mourish Kanna","Erode","Blue")
     var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
     lateinit var mySpinner: Spinner
-    lateinit var myListview: ListView
+    lateinit var myRecycler: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        mySpinner = findViewById(R.id.spinner) //taking handle
-        myListview = findViewById(R.id.listView2)
-        myListview.isClickable = true
+        mySpinner = findViewById(R.id.spinner)
+        myRecycler = findViewById(R.id.recyclerView)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         mySpinner.onItemSelectedListener = this
-        myListview.setOnItemClickListener(this)
 //        var data = intent.extras?.getString("nkey")
 //        Log.i("HomeActivity","data is = "+data)
 //        val homeTextView: TextView = findViewById(R.id.HomeTV)
@@ -44,8 +43,4 @@ class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener, Ad
             TODO("Not yet implemented")
     }
 
-    override fun onItemClick(adpater: AdapterView<*>?, view: View?, position: Int, id: Long) {
-            var item:String = adpater?.getItemAtPosition(position).toString()
-            Log.i(TAG, item )
-    }
 }
