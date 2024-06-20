3ceb79ea0fa4b8b625f80233996ea919be60ea2b
Mourish Kanna <mourishkumar1201@gmail.com>
2024-06-20 16:32:29 +0530

    Kotlin Example Code

---
+++ app/src/main/java/com/example/mourishkannatraining/kotlinexamples/Sample.kt
@@ -0,0 +1,11 @@
+package com.example.mourishkannatraining.kotlinexamples
+
+fun main() {
+    val numberOfFish = 50
+    val numberOfPlants = 23
+    if (numberOfFish > numberOfPlants) {
+        println("Good ratio!")
+    } else {
+        println("Unhealthy ratio")
+    }
+}
