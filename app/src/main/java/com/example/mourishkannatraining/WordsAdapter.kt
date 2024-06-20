3bc4901b14b3c79fa8357fcba6370daffbad9c4b
Mourish Kanna <mourishkumar1201@gmail.com>
2024-06-21 01:00:48 +0530

    Recycler View: Implementing Suggestions

--- app/src/main/java/com/example/mourishkannatraining/WordsAdapter.kt
+++ app/src/main/java/com/example/mourishkannatraining/WordsAdapter.kt
@@ -1,6 +1,24 @@
 package com.example.mourishkannatraining
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-class WordsAdapter : RecyclerView.Adapter<WordsViewHolder> {
+class WordsAdapter : RecyclerView.Adapter<WordsAdapter.WordsViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordsViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: WordsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class WordsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
 }
