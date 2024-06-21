66b77227aa120295990f7fbc175ff1957f6a4b84
Mourish Kanna <mourishkumar1201@gmail.com>
2024-06-21 11:41:38 +0530

    Sending parsed data to Recycler View

--- app/src/main/java/com/example/mourishkannatraining/HomeActivity.kt
+++ app/src/main/java/com/example/mourishkannatraining/HomeActivity.kt
@@ -8,15 +8,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mourishkannatraining.network.MarsApi
+import com.example.mourishkannatraining.network.MarsPhoto
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
+    var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
+
+    lateinit var marsRecyclerView: RecyclerView
+    lateinit var marsAdapter: MarsAdapter
+    lateinit var photos:List<MarsPhoto>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+        marsRecyclerView = findViewById(R.id.recyclerViewURLs)
+        marsRecyclerView.layoutManager = LinearLayoutManager(this)
+        photos = ArrayList()
+        marsAdapter = MarsAdapter(photos)
+        marsRecyclerView.adapter = marsAdapter
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -29,6 +43,8 @@ class HomeActivity : AppCompatActivity() {
     private fun getMarsPhotos() {
         GlobalScope.launch {
             var listMarsPhotos =   MarsApi.retrofitService.getPhotos()
+            photos = listMarsPhotos
+            marsAdapter.notifyDataSetChanged()
             var tvHome:TextView = findViewById(R.id.HomeTV)
 //            tvHome.setText(listMarsPhotos.get(1).imgSrc)
             Log.i("homeactiviy",listMarsPhotos.size.toString())
