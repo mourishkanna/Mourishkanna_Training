03bb2517962f9326d658e1f84df43d9acff87187
Mourish Kanna S <mourishkumar1201@gmail.com>
2024-06-28 23:51:54 +0530

    Notifications

--- app/src/main/java/com/example/mourishkannatraining/MainActivity.kt
+++ app/src/main/java/com/example/mourishkannatraining/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.mourishkannatraining
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.AlarmClock
 import android.util.Log
@@ -10,6 +15,7 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -70,7 +76,39 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
         // }
     }
+    fun showNotification(view: View) {
+        createNotificationChannel()
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
 
+        var builder = NotificationCompat.Builder(this, "promotions")
+            .setSmallIcon(R.drawable.baseline_school_24)
+            .setContentTitle("Mourish Kanna")
+            .setContentText("Demo Message")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+        var notifManager:NotificationManager =  getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notifManager.notify(12,builder.build())
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "channelname"
+            val descriptionText = "channel description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("promotions", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system.
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
     override fun onStart() {
         super.onStart()
         Log.e(TAG,"activity is started")
